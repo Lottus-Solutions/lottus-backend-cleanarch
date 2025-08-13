70aef923870b81409a8492663c3d885f14e982f8
EdsonUso <edson.cruz@sptech.school>
2025-08-13 19:31:29 -0300

    :rocket: - Inicializando projeto

---
+++ src/test/java/br/com/lottus/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lottus.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
