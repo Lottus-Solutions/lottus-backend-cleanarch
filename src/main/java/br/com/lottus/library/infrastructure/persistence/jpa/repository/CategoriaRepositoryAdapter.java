f92609435c7eef61e977bc720f8994051e3b0661
nathannolacio <nathannolacio04@gmail.com>
2025-09-04 21:34:43 -0300

    :bug: fix: endpoint para excluir categoria nao estava funcionando - corrigido

--- src/main/java/br/com/lottus/library/infrastructure/persistence/jpa/repository/CategoriaRepositoryAdapter.java
+++ src/main/java/br/com/lottus/library/infrastructure/persistence/jpa/repository/CategoriaRepositoryAdapter.java
@@ -58,6 +58,6 @@ public class CategoriaRepositoryAdapter implements CategoriaRepositoryPort {
 
     @Override
     public void deleteById(Long id) {
-
+        repository.deleteById(id);
     }
 }
--- src/main/java/br/com/lottus/library/infrastructure/web/controller/ControllerHandler.java
+++ src/main/java/br/com/lottus/library/infrastructure/web/controller/ControllerHandler.java
@@ -1,6 +1,7 @@
 package br.com.lottus.library.infrastructure.web.controller;
 
 import br.com.lottus.library.application.exceptions.CategoriaJaExistenteException;
+import br.com.lottus.library.application.exceptions.CategoriaNaoEncontradaException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,12 +13,21 @@ import java.util.Map;
 public class ControllerHandler {
 
     @ExceptionHandler(CategoriaJaExistenteException.class)
-    public ResponseEntity<Map<String, String>> CategoriaJaExistenteException(CategoriaJaExistenteException exception){
+    public ResponseEntity<Map<String, String>> categoriaJaExistenteException(CategoriaJaExistenteException exception){
         Map<String, String> error =Map.of(
                 "error","Ocorreu um erro de duplicidade: %s".formatted(exception.getMessage())
         );
 
         return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
+    @ExceptionHandler(CategoriaNaoEncontradaException.class)
+    public ResponseEntity<Map<String, String>> categoriaNaoEncontradaException(CategoriaNaoEncontradaException exception) {
+        Map<String, String> error = Map.of(
+                "error", exception.getMessage()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
 }
