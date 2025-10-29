6b5067bff20e76eca5f83a03d734048fc04d6d3d
nathannolacio <nathannolacio04@gmail.com>
2025-10-29 16:34:42 -0300

    :wrench: fix: Add exceptions no handler

--- src/main/java/br/com/lottus/library/infrastructure/web/controller/ControllerHandler.java
+++ src/main/java/br/com/lottus/library/infrastructure/web/controller/ControllerHandler.java
@@ -2,10 +2,7 @@
 package br.com.lottus.library.infrastructure.web.controller;
 
 import br.com.lottus.library.application.exceptions.*;
-import br.com.lottus.library.domain.exceptions.EmailInvalidoException;
-import br.com.lottus.library.domain.exceptions.IdAvatarInvalidoException;
-import br.com.lottus.library.domain.exceptions.NomeInvalidoException;
-import br.com.lottus.library.domain.exceptions.SenhaInvalidaException;
+import br.com.lottus.library.domain.exceptions.*;
 import br.com.lottus.library.infrastructure.web.dto.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -53,6 +50,106 @@ public class ControllerHandler {
     @ExceptionHandler({NomeInvalidoException.class, EmailInvalidoException.class, SenhaInvalidaException.class, IdAvatarInvalidoException.class})
     public ResponseEntity<ErrorResponse> handleValidationExceptions(RuntimeException ex, HttpServletRequest request) {
         return buildErrorResponse(HttpStatus.BAD_REQUEST, "Erro de validação", ex.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(AutorLivroVazioOuNuloException.class)
+    public ResponseEntity<ErrorResponse> handleAutorLivroVazioOuNulo(AutorLivroVazioOuNuloException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, "O nome do autor não pode ser nulo ou vazio", e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(CorCategoriaVazioOuNuloException.class)
+    public ResponseEntity<ErrorResponse> handleCorCategoriaVazioOuNulo(CorCategoriaVazioOuNuloException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, "A categoria não deve ser nula ou vazia", e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(DescricaoLivroMuitoLongaException.class)
+    public ResponseEntity<ErrorResponse> handleDescricaoLivroMuitoLonga(DescricaoLivroMuitoLongaException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, "A descrição não pode ter mais de 500 caracteres", e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(EmailInvalidoException.class)
+    public ResponseEntity<ErrorResponse> handleEmailInvalidoException(EmailInvalidoException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, "Email inválido", e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(FinalidadeNaoInformadaException.class)
+    public ResponseEntity<ErrorResponse> handleFinalidadeNaoInformada(FinalidadeNaoInformadaException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, "A finalidade do arquivo não foi informada.", e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(IdAvatarInvalidoException.class)
+    public ResponseEntity<ErrorResponse> handleIdAvatarInvalido(IdAvatarInvalidoException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, "ID do avatar inválido.", e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(NomeCategoriaVazioOuNuloException.class)
+    public ResponseEntity<ErrorResponse> handleNomeCategoriaVazioOuNulo(NomeCategoriaVazioOuNuloException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, "Nome não pode ser nulo ou vazio.", e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(NomeLivroVazioOuNuloException.class)
+    public ResponseEntity<ErrorResponse> handleNomeLivroVazioOuNulo(NomeCategoriaVazioOuNuloException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, "Nome da categoria não pode ser nulo ou vazio.", e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(QuantidadeDisponivelInvalidaException.class)
+    public ResponseEntity<ErrorResponse> handleQuantidadeDisponivelInvalida(QuantidadeDisponivelInvalidaException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, "Quantidade disponível inválida.", e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(QuantidadeLivroInvalidaException.class)
+    public ResponseEntity<ErrorResponse> handleQuantidadeLivroInvalida(QuantidadeLivroInvalidaException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, "A quantidade do livro não pode ser nula ou menor que um.", e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(StatusInvalidoException.class)
+    public ResponseEntity<ErrorResponse> handleStatusInvalidoException(StatusInvalidoException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, "Status inválido", e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(AlunoJaPossuiEmprestimoAtivoException.class)
+    public ResponseEntity<ErrorResponse> handleAlunoJaPossuiEmprestimoAtivo(AlunoJaPossuiEmprestimoAtivoException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.CONFLICT, "O aluno já possui um empréstimo ativo e não pode realizar outro.", e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(AlunoNaoEncontradoException.class)
+    public ResponseEntity<ErrorResponse> handleAlunoNaoEncontrado(AlunoNaoEncontradoException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, "Aluno não encontrado.", e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(EmprestimoNaoEncontradoException.class)
+    public ResponseEntity<ErrorResponse> handleEmprestimoNaoEncontrado(EmprestimoNaoEncontradoException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, "Empréstimo não encontrado.", e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(FormatoArquivoInvalidoException.class)
+    public ResponseEntity<ErrorResponse> handleFormatoArquivoInvalidoException(FormatoArquivoInvalidoException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, "Formato do arquivo inválido.", e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(LivroIndisponivelException.class)
+    public ResponseEntity<ErrorResponse> handleLivroIndisponivel(LivroIndisponivelException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, "Não há unidades disponíveis deste livro para empréstimo.", e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(NenhumAlunoEncontradoException.class)
+    public ResponseEntity<ErrorResponse> handleNenhumAlunoEncontrado(NenhumAlunoEncontradoException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, "Nenhum aluno foi encontrado para os critérios fornecidos.", e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(TurmaJaCadastradaException.class)
+    public ResponseEntity<ErrorResponse> handleTurmaJaCadastrada(TurmaJaCadastradaException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.CONFLICT, "Uma turma com esta série já foi cadastrada.", e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(TurmaNaoEncontradaException.class)
+    public ResponseEntity<ErrorResponse> handleTurmaNaoEncontrada(TurmaNaoEncontradaException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, "Turma não encontrada.", e.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler(UsuarioNaoEncontradoException.class)
+    public ResponseEntity<ErrorResponse> handleUsuarioNaoEncontrado(UsuarioNaoEncontradoException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, "Usuario não encontrado!",e.getMessage(), request.getRequestURI());
     }
 
     @ExceptionHandler(Exception.class)
