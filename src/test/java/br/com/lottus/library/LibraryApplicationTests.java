443a439c2289ec3655e744c29c5224d52167b452
nathannolacio <nathannolacio04@gmail.com>
2025-12-04 14:33:05 -0300

    :wrench: fix: Add config para utilizacao do H2 para testes

--- src/test/java/br/com/lottus/library/LibraryApplicationTests.java
+++ src/test/java/br/com/lottus/library/LibraryApplicationTests.java
@@ -3,9 +3,11 @@ package br.com.lottus.library;
 import br.com.lottus.library.infrastructure.storage.LocalFileStorageAdapter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LibraryApplicationTests {
 
     @MockitoBean
