94f56266f0c4ac03bb0be28929bfd17e27f64f7a
nathannolacio <nathannolacio04@gmail.com>
2025-12-04 13:32:29 -0300

    :wrench: fix: Config CI

--- src/test/java/br/com/lottus/library/LibraryApplicationTests.java
+++ src/test/java/br/com/lottus/library/LibraryApplicationTests.java
@@ -1,10 +1,15 @@
 package br.com.lottus.library;
 
+import br.com.lottus.library.infrastructure.storage.LocalFileStorageAdapter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class LibraryApplicationTests {
+
+    @MockitoBean
+    private LocalFileStorageAdapter localFileStorageAdapter;
 
     @Test
     void contextLoads() {
